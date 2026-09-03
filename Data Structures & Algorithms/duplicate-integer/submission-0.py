5ba066246927c4557597f2144160e130f9e53532
David Serrudo <dserrudocaina@gmail.com>
2026-09-03 19:43:21 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        visto = set()
+        for n in nums:
+            if n in visto:
+                return True
+            visto.add(n)
+        return False
